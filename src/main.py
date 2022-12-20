5e1289637a89525cf2c3c822e6f1da7319313bd5
seb-viney <sebastian.r.viney@gmail.com>
2022-12-20 15:47:49 +1100

    get_price_history being used in main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@
 
 
 # Local import
+from src.functions import price_history
 
 
 def main():
-    print('')
+
+    data = price_history.get_price_history(ticker_list=['AAPL', 'MSFT'], save_csv=True)
 
 if __name__ == "__main__":
     main()
