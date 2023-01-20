a5ba9be580bf48fc0fcc66f9f217970c0528898e
seb-viney <sebastian.r.viney@gmail.com>
2023-01-20 15:43:11 +1100

    updating docstring

--- src/functions/data_visualization.py
+++ src/functions/data_visualization.py
@@ -6,9 +6,11 @@ import seaborn as sns
 def line_chart(dataframe, hue="Stock Code", x_axis="Date", y_axis="Open"):
     
     '''
-    example call: 
-    
+    example call: data_visualization.line_chart(dataframe=data[["Open","Stock Code"]])
+    dataframe: The data
+    hue: Which column in the data to compare
     '''
+    
     sns.set_theme(style="darkgrid", palette="pastel")
     sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
     sns.lineplot(data=dataframe, x=x_axis, y=y_axis, hue=hue)
